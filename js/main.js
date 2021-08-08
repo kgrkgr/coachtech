a79464c93a4f8283a1b5bb32a01d43132174e184
kgrkgr <kggrrr5@gmail.com>
2021-08-08 09:55:07 +0900

    1

---
+++ js/main.js
@@ -0,0 +1,19 @@
+const items = [
+  { name: "水", price: 100, },
+  { name: "リンゴジュース", price: 130, },
+  { name: "コーヒー", price: 150, },
+  { name: "モンスター", price: 200, },
+  { name: "レッドブル（大）", price: 250, },
+];
+
+const buy = function (pay, itemName) {
+  const findItem = items.find((i) => i.name == itemName);
+  if (!findItem) return console.log("その商品は存在しません。");
+  if (pay < findItem.price) return console.log("お金が足りません。");
+  const change = pay - findItem.price;
+  console.log(findItem.name + "をお買い上げありがとうございます");
+  if (change <= 0) {
+    return console.log("おつりはありません。");
+  }
+  console.log("おつりは" + change + "円になります。");
+}
